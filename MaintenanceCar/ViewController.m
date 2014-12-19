3345ddba8b21f4b3b730e03c3a989c8ae43986af
RockerHX <RockerHX@gmail.com>
2014-12-19 11:11:32 +0800

    Initial Commit Master branch

---
+++ MaintenanceCar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MaintenanceCar
+//
+//  Created by ShiCang on 14/12/19.
+//  Copyright (c) 2014年 MaintenanceCar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios, '6.0'
+pod 'ASIHTTPRequest', '~> 1.8.2'
+pod 'MBProgressHUD', '~> 0.9'
