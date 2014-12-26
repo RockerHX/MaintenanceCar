18edf803ef91484c58e6dc6c9461e7808ab3935d
RockerHX <RockerHX@gmail.com>
2014-12-26 11:26:43 +0800

    添加第三方库友盟统计SDK

--- Podfile
+++ Podfile
@@ -4,3 +4,7 @@ platform :ios, '7.0'
 pod 'AFNetworking', '~> 2.5.0'
 pod 'JSONModel', '~> 1.0.1'
 pod 'MBProgressHUD', '~> 0.9'
+
+# ********************** 第三方SDK **********************
+# 友盟统计SDK
+pod 'UMengAnalytics', '~> 3.1.8'
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -60,3 +60,9 @@
 #define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
 
+// UMengAnalytics
+#define COCOAPODS_POD_AVAILABLE_UMengAnalytics
+#define COCOAPODS_VERSION_MAJOR_UMengAnalytics 3
+#define COCOAPODS_VERSION_MINOR_UMengAnalytics 1
+#define COCOAPODS_VERSION_PATCH_UMengAnalytics 8
+
