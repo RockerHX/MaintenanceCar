8c96d5000bfdee877c22558bcd4bbed66b079848
RockerHX <RockerHX@gmail.com>
2014-12-27 19:57:05 +0800

    完成商户列表模块 1.商户列表数据请求 2.商户列表布局

--- MaintenanceCar/ViewControllers/SCMerchantViewController.m
+++ MaintenanceCar/ViewControllers/SCMerchantViewController.m
@@ -8,8 +8,20 @@
 
 #import "SCMerchantViewController.h"
 #import <UMengAnalytics/MobClick.h>
+#import <MBProgressHUD/MBProgressHUD.h>
+#import "MicroCommon.h"
+#import "SCAPIRequest.h"
+#import "SCMerchant.h"
+#import "SCMerchantTableViewCell.h"
+#import "SCLocationInfo.h"
+#import "SCMerchantList.h"
+
+#define MerchantCellReuseIdentifier     @"MerchantCellReuseIdentifier"
 
 @interface SCMerchantViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSMutableArray *merchantList;
+@property (nonatomic, assign) NSInteger      offset;
 
 @end
 
@@ -33,14 +45,9 @@
 {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
-    self.tableView.delegate = self;
-    self.tableView.dataSource = self;
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [self initConfig];
+    [self startMerchantListRequest];
 }
 
 - (void)didReceiveMemoryWarning
@@ -53,14 +60,17 @@
 #pragma mark -
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return [SCMerchantList shareList].items.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuseIdentifier" forIndexPath:indexPath];
+    SCMerchantTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MerchantCellReuseIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
+    SCMerchant *merchant = [SCMerchantList shareList].items[indexPath.row];
+    cell.merchantNameLabel.text = merchant.detail.name;
+    cell.distanceLabel.text = merchant.distance;
     
     return cell;
 }
@@ -70,6 +80,47 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - Private Methods
+#pragma mark -
+- (void)initConfig
+{
+    _offset               = 0;
+    _merchantList         = [@[] mutableCopy];
+
+    _tableView.delegate   = self;
+    _tableView.dataSource = self;
+}
+
+- (void)startMerchantListRequest
+{
+    SCLocationInfo *locationInfo = [SCLocationInfo shareLocationInfo];
+    NSDictionary *parameters     = @{@"word"        : locationInfo.city,
+                                     @"limit"       : @(MerchantListLimit),
+                                     @"offset"      : @(_offset),
+                                     @"radius"      : @(MerchantListRadius),
+                                     @"longtitude"  : locationInfo.longitude,
+                                     @"latitude"    : locationInfo.latitude};
+    
+    [[SCAPIRequest manager] startMerchantListAPIRequestWithParameters:parameters Success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SCLog(@"%@", responseObject);
+        NSArray *list = [[responseObject objectForKey:@"result"] objectForKey:@"items"];
+        
+        [list enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            NSError *error       = nil;
+            SCMerchant *merchant = [[SCMerchant alloc] initWithDictionary:obj error:&error];
+            SCError(@"weather model parse error:%@", error);
+            [_merchantList addObject:merchant];
+        }];
+        
+        [SCMerchantList shareList].items = _merchantList;
+        [MBProgressHUD hideHUDForView:self.view animated:YES];
+        [_tableView reloadData];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        SCError(@"%@", error);
+        [MBProgressHUD hideHUDForView:self.view animated:YES];
+    }];
 }
 
 @end
