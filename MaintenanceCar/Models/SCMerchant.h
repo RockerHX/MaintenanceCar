6ddac61aeccbc7d382b6780476efb94baa59a24e
RockerHX <RockerHX@gmail.com>
2014-12-27 19:07:32 +0800

    商户Model新增： 1.detail - 商户详情属性，distance - 商户距离属性 2.增加displayDistanceWithServerDistance方法用于处理APP上显示的距离

--- MaintenanceCar/Models/SCMerchant.h
+++ MaintenanceCar/Models/SCMerchant.h
@@ -9,12 +9,18 @@
 #import "JSONModel.h"
 #import "SCMerchantDetail.h"
 
+// 商户Model
 @interface SCMerchant : JSONModel
 
-@property (nonatomic, strong) NSArray          *attribute;
-@property (nonatomic, strong) SCMerchantDetail *fields;
-@property (nonatomic, strong) NSArray          *property;
-@property (nonatomic, strong) NSArray          *sortExprValues;
-@property (nonatomic, strong) NSDictionary     *variableValue;
+// 以下属性供Model解析，不建议调用
+@property (nonatomic, strong) NSArray                    *attribute;
+@property (nonatomic, strong) SCMerchantDetail           *fields;
+@property (nonatomic, strong) NSArray                    *property;
+@property (nonatomic, strong) NSArray                    *sortExprValues;
+@property (nonatomic, strong) NSDictionary               *variableValue;
+
+// 以下是建议调用的属性
+@property (nonatomic, strong) SCMerchantDetail<Optional> *detail;           // 商户的详细信息
+@property (nonatomic, copy)   NSString<Optional>         *distance;         // 手机当前位置与商户的距离
 
 @end
--- MaintenanceCar/Models/SCMerchant.m
+++ MaintenanceCar/Models/SCMerchant.m
@@ -7,7 +7,62 @@
 //
 
 #import "SCMerchant.h"
+#import "MicroCommon.h"
+
+@interface SCMerchant ()
+{
+    double _serverDistance;     // 根据自己位置从服务器请求回来到商户的距离
+}
+
+@end
 
 @implementation SCMerchant
+
+#pragma mark - Private Methods
+#pragma mark -
+/**
+ *  得到用于APP上显示的实际距离（实际距离小于1000米时显示单位为米M，精确度为10米；大于1000米是显示单位为千米KM）
+ *
+ *  @param serverDistance 从服务器上请求得到的距离
+ *
+ *  @return 用于APP显示的距离
+ */
+- (NSString *)displayDistanceWithServerDistance:(double)serverDistance
+{
+    NSString *displayDistance = @"";
+    if (serverDistance < 1.0f)
+    {
+        NSInteger distance = [[NSString stringWithFormat:@"%.2f", serverDistance] doubleValue] * 1000;
+        displayDistance = [NSString stringWithFormat:@"%ldm", (long)distance];
+    }
+    else
+    {
+        displayDistance = [NSString stringWithFormat:@"%.1fkm", serverDistance];
+    }
+    return displayDistance;
+}
+
+#pragma mark - Getter Methods
+#pragma mark -
+- (NSString *)distance
+{
+    @try {
+        _serverDistance = [_sortExprValues[0] doubleValue];
+        SCLog(@"%@", _sortExprValues[0]);
+    }
+    @catch (NSException *exception) {
+        _serverDistance = 0.0f;
+        SCException(@"SCMerchant object get serverDistance exception reason:%@", exception.reason);
+    }
+    @finally {
+        SCLog(@"%f", _serverDistance);
+    }
+    return [self displayDistanceWithServerDistance:_serverDistance];
+}
+
+- (SCMerchantDetail *)detail
+{
+    return _fields;
+}
 
 @end
