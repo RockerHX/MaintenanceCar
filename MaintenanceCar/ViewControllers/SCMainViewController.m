97f56c50ffed6a761c2e631a249fb5f74fb2be6a
RockerHX <RockerHX@gmail.com>
2015-04-15 18:42:53 +0800

    Update

--- MaintenanceCar/ViewControllers/SCMainViewController.m
+++ MaintenanceCar/ViewControllers/SCMainViewController.m
@@ -18,15 +18,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
     [self initConfig];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Private Methods
