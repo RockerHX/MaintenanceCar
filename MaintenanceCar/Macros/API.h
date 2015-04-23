bfa0f7cb55b6512312202cbaca4b86cffaf6cdba
RockerHX <RockerHX@gmail.com>
2015-04-23 18:24:07 +0800

    Update: 1.API分版本请求

--- MaintenanceCar/Macros/API.h
+++ MaintenanceCar/Macros/API.h
@@ -26,8 +26,10 @@
 
 
 #pragma mark - API Path
-#define APIPath             @"/v2"                                          // 接口路径
-#define APIURL              [DoMain stringByAppendingString:APIPath]        // 接口链接
+#define V1APIPath           @"/v1"                                          // V1接口路径
+#define V2APIPath           @"/v2"                                          // V2接口路径
+#define V1APIURL            [DoMain stringByAppendingString:V1APIPath]      // V1接口链接
+#define V2APIURL            [DoMain stringByAppendingString:V2APIPath]      // V2接口链接
 #define ImagePath           @"/brand/brand_"                                // 图片资源路径
 #define ImageURL            [ImageDoMain stringByAppendingString:ImagePath] // 图片资源链接
 
@@ -84,55 +86,55 @@
 
 
 #pragma mark - API URL
-#define WearthAPIURL                    [APIURL stringByAppendingString:WearthAPI]                  // 天气接口URL - 用于主页模块获取天气信息
-#define SearchAPIURL                    [APIURL stringByAppendingString:SearchAPI]                  // 商家搜索接口URL - 用于商家搜索和筛选
-#define OperateSearchAPIURL             [APIURL stringByAppendingString:OperateSearchAPI]           // 运营位商家搜索接口URL - 用于运营位商家搜索和筛选
-#define MerchantDetailAPIURL            [APIURL stringByAppendingString:MerchantDetailAPI]          // 商家详情接口URL - 用于获取短信或者语音验证码
-#define MerchantCollectionAPIURL        [APIURL stringByAppendingString:MerchantCollectionAPI]      // 商家收藏接口URL - 用于商家收藏和获取商家收藏
-#define CancelCollectionAPIURL          [APIURL stringByAppendingString:CancelCollectionAPI]        // 取消商家收藏接口URL - 用于商家详情页面取消收藏或者个人中心页面删除收藏
-#define CheckMerchantCollectionAPIURL   [APIURL stringByAppendingString:CheckMerchantCollectionAPI] // 检查商家收藏状态接口URL - 用于商家详情页面检查商家收藏状态
-#define MerchantReservationAPIURL       [APIURL stringByAppendingString:MerchantReservationAPI]     // 商家预约接口URL - 用于商家预约项目
+#define WearthAPIURL                    [V1APIURL stringByAppendingString:WearthAPI]                  // 天气接口URL - 用于主页模块获取天气信息
+#define SearchAPIURL                    [V1APIURL stringByAppendingString:SearchAPI]                  // 商家搜索接口URL - 用于商家搜索和筛选
+#define OperateSearchAPIURL             [V1APIURL stringByAppendingString:OperateSearchAPI]           // 运营位商家搜索接口URL - 用于运营位商家搜索和筛选
+#define MerchantDetailAPIURL            [V1APIURL stringByAppendingString:MerchantDetailAPI]          // 商家详情接口URL - 用于获取短信或者语音验证码
+#define MerchantCollectionAPIURL        [V1APIURL stringByAppendingString:MerchantCollectionAPI]      // 商家收藏接口URL - 用于商家收藏和获取商家收藏
+#define CancelCollectionAPIURL          [V1APIURL stringByAppendingString:CancelCollectionAPI]        // 取消商家收藏接口URL - 用于商家详情页面取消收藏或者个人中心页面删除收藏
+#define CheckMerchantCollectionAPIURL   [V1APIURL stringByAppendingString:CheckMerchantCollectionAPI] // 检查商家收藏状态接口URL - 用于商家详情页面检查商家收藏状态
 
-#define MerchantGroupProductAPIURL      [APIURL stringByAppendingString:MerchantGroupProductAPI]    // 商家团购详情接口URL - 用于商家团购项目
-#define WeiXinPayAPIURL                 [APIURL stringByAppendingString:WeiXinPayAPI]               // 微信支付订单接口URL - 用于团购支付时获取微信支付订单信息
-#define AliPayAPIURL                    [APIURL stringByAppendingString:AliPayAPI]                  // 支付宝钱包支付订单接口URL - 用于团购支付时获取支付宝钱包订单信息
-#define GenerateCouponAPIURL            [APIURL stringByAppendingString:GenerateCouponAPI]          // 生成团购券接口URL - 用于付款成功后生成团购券
-#define MyGroupProductAPIURL            [APIURL stringByAppendingString:MyGroupProductAPI]          // 我的团购券列表接口URL - 用于获取用户所有团购券列表
-#define CouponDetailAPIURL              [APIURL stringByAppendingString:CouponDetailAPI]            // 团购券详情接口URL - 用于获取团购券详情
-#define CouponRefundAPIURL              [APIURL stringByAppendingString:CouponRefundAPI]            // 团购券退款的接口URL - 用于团购券详情申请退款
+#define MerchantGroupProductAPIURL      [V1APIURL stringByAppendingString:MerchantGroupProductAPI]    // 商家团购详情接口URL - 用于商家团购项目
+#define WeiXinPayAPIURL                 [V1APIURL stringByAppendingString:WeiXinPayAPI]               // 微信支付订单接口URL - 用于团购支付时获取微信支付订单信息
+#define AliPayAPIURL                    [V1APIURL stringByAppendingString:AliPayAPI]                  // 支付宝钱包支付订单接口URL - 用于团购支付时获取支付宝钱包订单信息
+#define GenerateCouponAPIURL            [V1APIURL stringByAppendingString:GenerateCouponAPI]          // 生成团购券接口URL - 用于付款成功后生成团购券
+#define MyGroupProductAPIURL            [V2APIURL stringByAppendingString:MyGroupProductAPI]          // 我的团购券列表接口URL - 用于获取用户所有团购券列表
+#define CouponDetailAPIURL              [V1APIURL stringByAppendingString:CouponDetailAPI]            // 团购券详情接口URL - 用于获取团购券详情
+#define CouponRefundAPIURL              [V1APIURL stringByAppendingString:CouponRefundAPI]            // 团购券退款的接口URL - 用于团购券详情申请退款
 
-#define CommentAPIURL                   [APIURL stringByAppendingString:CommentAPI]                 // 评价接口URL - 用于添加评价
-#define MerchantCommentAPIURL           [APIURL stringByAppendingString:MerchantCommentAPI]         // 商家评价接口URL - 用于获取商家评价列表
+#define CommentAPIURL                   [V1APIURL stringByAppendingString:CommentAPI]                 // 评价接口URL - 用于添加评价
+#define MerchantCommentAPIURL           [V1APIURL stringByAppendingString:MerchantCommentAPI]         // 商家评价接口URL - 用于获取商家评价列表
 
-#define VerificationCodeAPIURL          [APIURL stringByAppendingString:VerificationCodeAPI]        // 获取验证码接口URL - 用于获取短信或者语音验证码
-#define LoginAPIURL                     [APIURL stringByAppendingString:LoginAPI]                   // 用户登录接口URL - 用于用户登录
-#define UserLogAPIURL                   [APIURL stringByAppendingString:UserLogAPI]                 // 用户日志记录接口URL - 打开APP后如果用户已经登录异步调用此URL记录
-#define RefreshTokenAPIURL              [APIURL stringByAppendingString:RefreshTokenAPI]            // 刷新Token接口URL - 刷新token过期时间
+#define VerificationCodeAPIURL          [V1APIURL stringByAppendingString:VerificationCodeAPI]        // 获取验证码接口URL - 用于获取短信或者语音验证码
+#define LoginAPIURL                     [V1APIURL stringByAppendingString:LoginAPI]                   // 用户登录接口URL - 用于用户登录
+#define UserLogAPIURL                   [V1APIURL stringByAppendingString:UserLogAPI]                 // 用户日志记录接口URL - 打开APP后如果用户已经登录异步调用此URL记录
+#define RefreshTokenAPIURL              [V1APIURL stringByAppendingString:RefreshTokenAPI]            // 刷新Token接口URL - 刷新token过期时间
 
-#define MyReservationAPIURL             [APIURL stringByAppendingString:MyReservationAPI]           // 商家预约接口URL - 用于商家预约项目
-#define UpdateReservationAPIURL         [APIURL stringByAppendingString:UpdateReservationAPI]       // 更新预约接口URL - 用于用户取消预约项目
-#define ReservationItemNumAPIURL        [APIURL stringByAppendingString:ReservationItemNumAPI]      // 预约日期数量接口URL - 用于获取预约项目数量
+#define MerchantReservationAPIURL       [V2APIURL stringByAppendingString:MerchantReservationAPI]     // 商家预约接口URL - 用于商家预约项目
+#define MyReservationAPIURL             [V2APIURL stringByAppendingString:MyReservationAPI]           // 商家预约接口URL - 用于商家预约项目
+#define UpdateReservationAPIURL         [V2APIURL stringByAppendingString:UpdateReservationAPI]       // 更新预约接口URL - 用于用户取消预约项目
+#define ReservationItemNumAPIURL        [V2APIURL stringByAppendingString:ReservationItemNumAPI]      // 预约日期数量接口URL - 用于获取预约项目数量
 
-#define CarBrandAPIURL                  [APIURL stringByAppendingString:CarBrandAPI]                // 汽车品牌接口URL - 用于更新汽车品牌
-#define CarModelAPIURL                  [APIURL stringByAppendingString:CarModelAPI]                // 汽车型号接口URL - 用于更新汽车型号
-#define CarsAPIURL                      [APIURL stringByAppendingString:CarsAPI]                    // 汽车款式接口URL - 用于更新汽车款式
-#define AddCarAPIURL                    [APIURL stringByAppendingString:AddCarAPI]                  // 添加车辆接口URL - 用于用户添加车辆
-#define DeleteCarAPIURL                 [APIURL stringByAppendingString:DeleteCarAPI]               // 删除车辆接口URL - 用于用户删除车辆
+#define CarBrandAPIURL                  [V1APIURL stringByAppendingString:CarBrandAPI]                // 汽车品牌接口URL - 用于更新汽车品牌
+#define CarModelAPIURL                  [V1APIURL stringByAppendingString:CarModelAPI]                // 汽车型号接口URL - 用于更新汽车型号
+#define CarsAPIURL                      [V1APIURL stringByAppendingString:CarsAPI]                    // 汽车款式接口URL - 用于更新汽车款式
+#define AddCarAPIURL                    [V1APIURL stringByAppendingString:AddCarAPI]                  // 添加车辆接口URL - 用于用户添加车辆
+#define DeleteCarAPIURL                 [V1APIURL stringByAppendingString:DeleteCarAPI]               // 删除车辆接口URL - 用于用户删除车辆
 
-#define MaintenanceAPIURL               [APIURL stringByAppendingString:MaintenanceAPI]             // 保养数据接口URL - 用于获取用户车辆保养数据
-#define UpdateCarAPIURL                 [APIURL stringByAppendingString:UpdateCarAPI]               // 更新车辆数据接口URL - 用于用户更新车辆数据
+#define MaintenanceAPIURL               [V1APIURL stringByAppendingString:MaintenanceAPI]             // 保养数据接口URL - 用于获取用户车辆保养数据
+#define UpdateCarAPIURL                 [V1APIURL stringByAppendingString:UpdateCarAPI]               // 更新车辆数据接口URL - 用于用户更新车辆数据
 
-#define AllDictionaryAPIURL             [APIURL stringByAppendingString:AllDictionaryAPI]           // 所有数据字典接口URL
-#define FlagsColorExplainAPIURL         [APIURL stringByAppendingString:FlagsColorExplainAPI]       // 获取商家对应Flag颜色值接口URL
-#define MerchantTagsAPIURL              [APIURL stringByAppendingString:MerchantTagsAPI]            // 获取商家对应Tag标签接口URL
+#define AllDictionaryAPIURL             [V1APIURL stringByAppendingString:AllDictionaryAPI]           // 所有数据字典接口URL
+#define FlagsColorExplainAPIURL         [V1APIURL stringByAppendingString:FlagsColorExplainAPI]       // 获取商家对应Flag颜色值接口URL
+#define MerchantTagsAPIURL              [V1APIURL stringByAppendingString:MerchantTagsAPI]            // 获取商家对应Tag标签接口URL
 
 
-#define OperatADAPIURL                  [APIURL stringByAppendingString:OperatADAPI]                // 首页运营位接口URL - 用于首页运营位数据
-#define HomePageReservationAPIURL       [APIURL stringByAppendingString:HomePageReservationAPI]     // 最新预约信息接口URL - 用于首页获取
-#define HomePageSpecialAPIURL           [APIURL stringByAppendingString:HomePageSpecialAPI]         // 首页第四个按钮数据接口URL
+#define OperatADAPIURL                  [V1APIURL stringByAppendingString:OperatADAPI]                // 首页运营位接口URL - 用于首页运营位数据
+#define HomePageReservationAPIURL       [V1APIURL stringByAppendingString:HomePageReservationAPI]     // 最新预约信息接口URL - 用于首页获取
+#define HomePageSpecialAPIURL           [V1APIURL stringByAppendingString:HomePageSpecialAPI]         // 首页第四个按钮数据接口URL
 
 
 #pragma mark - V2
-#define MyOderAPIURL                    [APIURL stringByAppendingString:MyOderAPI]                  // 我的订单接口URL - 用于我的订单数据获取
+#define MyOderAPIURL                    [V2APIURL stringByAppendingString:MyOderAPI]                  // 我的订单接口URL - 用于我的订单数据获取
 
 #endif
