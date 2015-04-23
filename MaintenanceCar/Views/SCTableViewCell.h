3001110cda8e2e503984211de8a3320ad2680e4d
RockerHX <RockerHX@gmail.com>
2015-04-23 15:48:09 +0800

    Add: 1.SCTableViewCell作为V2版本TableViewCell的父类，提供处理cell的公用方法

---
+++ MaintenanceCar/Views/SCTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  SCTableViewCell.h
+//  MaintenanceCar
+//
+//  Created by ShiCang on 15/4/23.
+//  Copyright (c) 2015年 MaintenanceCar. All rights reserved.
+//
+
+#import "SCViewCategory.h"
+
+@interface SCTableViewCell : UITableViewCell
+
+/**
+ *  刷新Cell约束
+ *
+ *  @return 刷新后的Cell高度
+ */
+- (CGFloat)layoutSizeFittingSize;
+
+@end
---
+++ MaintenanceCar/Views/SCTableViewCell.m
@@ -0,0 +1,31 @@
+//
+//  SCTableViewCell.m
+//  MaintenanceCar
+//
+//  Created by ShiCang on 15/4/23.
+//  Copyright (c) 2015年 MaintenanceCar. All rights reserved.
+//
+
+#import "SCTableViewCell.h"
+
+@implementation SCTableViewCell
+
+#pragma mark - Setter And Getter Methods
+- (void)setFrame:(CGRect)frame
+{
+    frame.origin.x = 10.0f;
+    frame.origin.y = frame.origin.y + 5.0f;
+    frame.size.width = frame.size.width - 20.0f;
+    frame.size.height = frame.size.height - 10.0f;
+    [super setFrame:frame];
+}
+
+#pragma mark - Public Methods
+- (CGFloat)layoutSizeFittingSize
+{
+    [self updateConstraintsIfNeeded];
+    [self layoutIfNeeded];
+    return [self.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+}
+
+@end
