da229c2cb3682793ed2ef1c1263770a84ec12418
RockerHX <RockerHX@gmail.com>
2015-05-23 20:07:03 +0800

    Update: 1.第三方库

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -112,7 +112,7 @@
 #define COCOAPODS_POD_AVAILABLE_SCNumberKeyBoard
 #define COCOAPODS_VERSION_MAJOR_SCNumberKeyBoard 0
 #define COCOAPODS_VERSION_MINOR_SCNumberKeyBoard 1
-#define COCOAPODS_VERSION_PATCH_SCNumberKeyBoard 1
+#define COCOAPODS_VERSION_PATCH_SCNumberKeyBoard 2
 
 // UITableView+FDTemplateLayoutCell
 #define COCOAPODS_POD_AVAILABLE_UITableView_FDTemplateLayoutCell
