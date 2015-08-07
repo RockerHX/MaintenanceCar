08e0f0a5eb589b78ade05c46271a26b0d7f4b721
RockerHX <RockerHX@gmail.com>
2015-08-07 18:30:19 +0800

    Update

---
+++ MaintenanceCar/ViewControllers/Guide/SCGuideViewController.h
@@ -0,0 +1,17 @@
+//
+//  SCGuideViewController.h
+//  MaintenanceCar
+//
+//  Created by Andy on 15/8/7.
+//  Copyright (c) 2015年 MaintenanceCar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SCLoopScrollView;
+
+@interface SCGuideViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet SCLoopScrollView *guideView;
+
+@end
---
+++ MaintenanceCar/ViewControllers/Guide/SCGuideViewController.m
@@ -0,0 +1,19 @@
+//
+//  SCGuideViewController.m
+//  MaintenanceCar
+//
+//  Created by Andy on 15/8/7.
+//  Copyright (c) 2015年 MaintenanceCar. All rights reserved.
+//
+
+#import "SCGuideViewController.h"
+#import <SCLoopScrollView/SCLoopScrollView.h>
+
+@implementation SCGuideViewController
+
+#pragma mark - View Controller Life Cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
